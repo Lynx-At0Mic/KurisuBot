1e49f3957b811f527365b3ef80d610b31141fe61
Lynx <lynxat0mic02@gmail.com>
2020-08-11 02:27:07 +0100

    Added permission check to nuke.js

--- src/commands/adminUtils/nuke.js
+++ src/commands/adminUtils/nuke.js
@@ -1,4 +1,11 @@
 let NukeAsync = async(client, message, args, services) =>{
+
+    if(!message.channel.deletable){
+        let embed = services.CommandErrorEmbed()
+            .setDescription('Bot does no have permission to delete this channel.');
+            await message.channel.send(embed);
+    }
+
     let channelName = message.channel.name;
     let parent = message.channel.parent;
     let topic = message.channel.topic;
@@ -8,14 +15,18 @@ let NukeAsync = async(client, message, args, services) =>{
 
     let embed = services.InfoEmbed()
     .setTitle('NUKING CHANNEL!')
-    .setDescription('The channel will be nuked in 5 secconds');
+    .setDescription('The channel will be nuked in 3 secconds!');
 
     await message.channel.send(embed);
     setTimeout(async function(){
         await message.channel.delete();
-        if(!message.channel.deleted){await message.channel.send('Error deleting channel');}
+        if(!message.channel.deleted){
+            let embed = services.CommandErrorEmbed()
+            .setDescription('Error deleting channel!');
+            await message.channel.send(embed);
+        }
         await guild.channels.create(channelName, {parent: parent});
-    }, 5000);
+    }, 3000);
 }
 
 module.exports = {
