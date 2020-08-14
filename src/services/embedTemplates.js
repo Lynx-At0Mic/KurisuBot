08e4406e126bf81f45ce9dc8fbf701612b3c18aa
Lynx <lynxat0mic02@gmail.com>
2020-08-14 22:07:04 +0100

    Embed functions now intergrated into module.exports

--- src/services/embedTemplates.js
+++ src/services/embedTemplates.js
@@ -2,52 +2,40 @@ const Discord = require('discord.js');
 const footerlong = 'I am best girl. "But Moeka is.." Nope yandere-esk types are bad.\nNow that\'s a science fact for you.';
 const footershort = 'I am best girl';
 
-function CommandErrorEmbed(reason=null){
-    embed = new Discord.MessageEmbed()
-    .setTitle('Command Error')
-    .setColor('#FF0000')
-    .setDescription(reason);
-    // .setFooter(footershort);
-    return embed;
-}
-
-function CommandSuccessEmbed(reason=null){
-    embed = new Discord.MessageEmbed()
-    .setTitle('Success')
-    .setColor('#08FF00')
-    .setDescription(reason);
-    // .setFooter(footershort);
-    return embed;
-}
-
-function InfoEmbed() {
-    let embed = new Discord.MessageEmbed()
-    .setColor('#00A3F5')
-    .setFooter(footerlong);
-    return embed;
-}
-
-function HelpEmbed(){
-    let embed = new Discord.MessageEmbed()
-    .setColor('#9CF6F6')
-    .setFooter(footershort);
-    return embed;
-}
-
-function GameEmbed(){
-    let embed = new Discord.MessageEmbed()
-    .setColor('#4F359B')
-    .setFooter(footershort);
-    return embed;
-}
-
 module.exports = {
     footershort: footershort,
     footerlong: footerlong,
 
-    cmderrEmbed: CommandErrorEmbed,
-    cmdsuccessEmbed: CommandSuccessEmbed,
-    infoEmbed: InfoEmbed,
-    helpEmbed: HelpEmbed,
-    gameEmbed: GameEmbed
+    cmderrEmbed: function(reason=null){
+        embed = new Discord.MessageEmbed()
+        .setTitle('Command Error')
+        .setColor('#FF0000')
+        .setDescription(reason);
+        return embed;
+    },
+    cmdsuccessEmbed: function(reason=null){
+        embed = new Discord.MessageEmbed()
+        .setTitle('Success')
+        .setColor('#08FF00')
+        .setDescription(reason);
+        return embed;
+    },
+    infoEmbed: function(){
+        let embed = new Discord.MessageEmbed()
+        .setColor('#00A3F5')
+        .setFooter(footerlong);
+        return embed;
+    },
+    helpEmbed: function(){
+        let embed = new Discord.MessageEmbed()
+        .setColor('#9CF6F6')
+        .setFooter(footershort);
+        return embed;
+    },
+    gameEmbed: function(){
+        let embed = new Discord.MessageEmbed()
+        .setColor('#4F359B')
+        .setFooter(footershort);
+        return embed;
+    }
 }
